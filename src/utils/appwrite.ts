fc2302613c878e5a8a4d3efd8aacea6bf6378a73
ani <anujzzztw@gmail.com>
2023-05-30 22:04:32 +0530

    added the search bar

--- src/utils/appwrite.ts
+++ src/utils/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Account, ID, Databases, Avatars } from "appwrite";
+import { Client, Account, ID, Databases, Avatars, Functions } from "appwrite";
 
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
@@ -9,3 +9,5 @@ export const account = new Account(client);
 export const databases = new Databases(client);
 
 export const avatars = new Avatars(client);
+
+export const functions = new Functions(client);
