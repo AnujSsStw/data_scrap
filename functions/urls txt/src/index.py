7fcc4f2068fad4f79691114fef19ae6251bc193a
ani <anujzzztw@gmail.com>
2023-06-06 16:02:19 +0530

    update the json builder

--- functions/urls txt/src/index.py
+++ functions/urls txt/src/index.py
@@ -3,6 +3,8 @@ import praw
 import json
 import random
 import string
+import re
+import requests
 
 from appwrite.input_file import InputFile
 from appwrite.id import ID
@@ -70,38 +72,55 @@ def main(req, res):
   )
 
   # payload dict: {subreddits: str[], 4chan: str[], limit: int, userId: str{todo}, fileType: str{todo}}
-  # {
-  #   "subreddits": ["anime", "memes"],
+  # { 
+  #  "subreddits": ["anime", "memes"],
+  #   "4chan": ["b", "g"],
   #   "limit": 10,
   # }
-  actual_posts_url = []
   subreddits = None
+  chan4 = None
   try:
     payload = json.loads(req.payload)
     subreddits = payload['subreddits']
-    # limit = payload['limit']
+    chan4 = payload['4chan']
 
-    print(subreddits)
-
-    for subreddit in subreddits:
-      posts = reddit.subreddit(subreddit).hot(limit=payload['limit'])
-      for post in posts:
-        actual_posts_url.append(post.url)
-
-    print(actual_posts_url)
-    
-   # Create the file contents by joining array elements with newlines
-    file_contents = '\n'.join(actual_posts_url)
-    file = file_contents.encode('utf-8')
-
-    # # Create a file object with the contents
-    file_name = randomString() + '.txt'
-    result = storage.create_file('647a1723a374f6845b7a', ID.unique(), InputFile.from_bytes(file, file_name))
-
-    url = "https://cloud.appwrite.io/v1/storage/buckets/{}/files/{}/view?project={}".format(result['bucketId'], result["$id"], req.variables.get('APPWRITE_FUNCTION_PROJECT_ID'))
   except Exception as e:
     print(e)
 
+  my_dict = {
+    "reddit": {
+      "subreddits": subreddits,
+      "data": {
+        "image": [],
+        "video": [],
+        "gif": [],
+        "other": [],
+      },
+    },
+    "4chan": {
+      "boards": chan4,
+      "data": {
+        "image": [],
+        "video": [],
+        "gif": [],
+        "other": [],
+      },
+    },
+  }
+  
+  # for reddit
+  redditBuilder(subreddits, my_dict, reddit, payload)
+        
+  # for 4chan
+  chan4Builder(chan4, my_dict, payload)
+          
+# Create the file contents by joining array elements with newlines
+  file_contents = json.dumps(my_dict, indent=2)
+  file = file_contents.encode('utf-8')
+  # # Create a file object with the contents
+  file_name = randomString() + '.json'
+  result = storage.create_file('647a1723a374f6845b7a', ID.unique(), InputFile.from_bytes(file, file_name))
+  url = "https://cloud.appwrite.io/v1/storage/buckets/{}/files/{}/view?project={}".format(result['bucketId'], result["$id"], req.variables.get('APPWRITE_FUNCTION_PROJECT_ID'))
 
   #url example
   # https://cloud.appwrite.io/v1/storage/buckets/{647a1723a374f6 -> bucketId}/files/{647a1b7b31a -> fileId}/view?project={6463a34a73c& -> projectId}
@@ -115,3 +134,131 @@ def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
+
+def redditBuilder(subreddits: list, my_dict: dict, reddit: praw.Reddit, payload: dict):
+  for subreddit in subreddits:
+    postsHot = reddit.subreddit(subreddit).hot(limit=payload['limit']) 
+    postsNew = reddit.subreddit(subreddit).new(limit=payload['limit'])
+    postsRising = reddit.subreddit(subreddit).rising(limit=payload['limit'])
+    postsControversial = reddit.subreddit(subreddit).controversial(limit=payload['limit'])
+    postsTop = reddit.subreddit(subreddit).top(limit=payload['limit'])
+    pp = [postsHot, postsNew, postsRising, postsControversial, postsTop]
+
+    for postT in pp:
+      for post in postT:
+        
+        if re.search(r"\.(jpg|jpeg|png)$", post.url):
+          my_dict['reddit']['data']['image'].append(post.url)
+        elif re.search(r"\.(gif)$", post.url):
+          my_dict['reddit']['data']['gif'].append(post.url)
+        elif re.search(r"\.(mp4)$", post.url):
+          my_dict['reddit']['data']['video'].append(post.url)
+        else:
+          my_dict['reddit']['data']['other'].append(post.url)
+
+def chan4Builder(chan4: list, my_dict: dict, payload: dict):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+    }
+    print(chan4)
+    for board in chan4:
+        response = requests.get(
+            "https://a.4cdn.org/{}/catalog.json".format(board), headers=headers
+        ).json()
+
+        for page in response:
+            for thread in page["threads"]:
+              try:
+                  if "tim" in thread or "ext" in thread:
+                      if (
+                          thread["ext"] == ".jpg"
+                          or thread["ext"] == ".jpeg"
+                          or thread["ext"] == ".png"
+                          or thread["ext"] == ".webm"
+                      ) and (len(my_dict["4chan"]["data"]["image"]) < payload["limit"]):
+                          my_dict["4chan"]["data"]["image"].append(
+                              "https://i.4cdn.org/{}/{}{}".format(
+                                  board, thread["tim"], thread["ext"]
+                              )
+                          )
+                      elif thread["ext"] == ".mp4" and (
+                          len(my_dict["4chan"]["data"]["video"]) < payload["limit"]
+                      ):
+                          my_dict["4chan"]["data"]["video"].append(
+                              "https://i.4cdn.org/{}/{}{}".format(
+                                  board, thread["tim"], thread["ext"]
+                              )
+                          )
+                      elif thread["ext"] == ".gif" and (
+                          len(my_dict["4chan"]["data"]["gif"]) < payload["limit"]
+                      ):
+                          my_dict["4chan"]["data"]["gif"].append(
+                              "https://i.4cdn.org/{}/{}{}".format(
+                                  board, thread["tim"], thread["ext"]
+                              )
+                          )
+                  else:
+                      print("no image")
+                      if len(my_dict["4chan"]["data"]["text"]) < payload["limit"]:
+                          my_dict["4chan"]["data"]["text"].append(thread["com"])
+
+                  threadRes = requests.get(
+                      "https://a.4cdn.org/{}/thread/{}.json".format(board, thread["no"]),
+                      headers=headers,
+                  ).json()
+
+                  # taking almost 2 minutes to run
+                  # for post in threadRes["posts"]:
+                  #     if "tim" in post or "ext" in post:
+                  #         if (
+                  #             post["ext"] == ".jpg"
+                  #             or post["ext"] == ".jpeg"
+                  #             or post["ext"] == ".png"
+                  #             or post["ext"] == ".webm"
+                  #         ) and (
+                  #             len(my_dict["4chan"]["data"]["image"]) < payload["limit"]
+                  #         ):
+                  #             my_dict["4chan"]["data"]["image"].append(
+                  #                 "https://i.4cdn.org/{}/{}{}".format(
+                  #                     board, post["tim"], post["ext"]
+                  #                 )
+                  #             )
+                  #             if (
+                  #                 len(my_dict["4chan"]["data"]["image"])
+                  #                 >= payload["limit"]
+                  #             ):
+                  #                 break
+                  #         elif post["ext"] == ".mp4" and (
+                  #             len(my_dict["4chan"]["data"]["video"]) < payload["limit"]
+                  #         ):
+                  #             my_dict["4chan"]["data"]["video"].append(
+                  #                 "https://i.4cdn.org/{}/{}{}".format(
+                  #                     board, post["tim"], post["ext"]
+                  #                 )
+                  #             )
+
+                  #             if (
+                  #                 len(my_dict["4chan"]["data"]["video"])
+                  #                 >= payload["limit"]
+                  #             ):
+                  #                 break
+                  #         elif post["ext"] == ".gif" and (
+                  #             len(my_dict["4chan"]["data"]["gif"]) < payload["limit"]
+                  #         ):
+                  #             my_dict["4chan"]["data"]["gif"].append(
+                  #                 "https://i.4cdn.org/{}/{}{}".format(
+                  #                     board, post["tim"], post["ext"]
+                  #                 )
+                  #             )
+
+                  #             if len(my_dict["4chan"]["data"]["gif"]) >= payload["limit"]:
+                  #                 break
+
+                  #     else:
+                  #         if len(my_dict["4chan"]["data"]["text"]) >= payload["limit"]:
+                  #             break
+                  #         if len(my_dict["4chan"]["data"]["text"]) < payload["limit"]:
+                  #             my_dict["4chan"]["data"]["text"].append(post["com"])
+              except Exception as e:
+                  print(e)
+                  continue
