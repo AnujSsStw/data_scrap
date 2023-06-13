19d2b03ac0a9a50a620b7bc9b142e6f80bec73c4
ani <anujzzztw@gmail.com>
2023-06-13 16:48:48 +0530

    add imge

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,8 @@ import {
   Text,
   Heading,
   Button,
+  Image,
+  Box,
 } from "@chakra-ui/react";
 import { useAtom } from "jotai";
 import { payloadForL1 } from "~/context";
@@ -14,10 +16,14 @@ export const CardBox = ({
   title,
   discription,
   source,
+  media,
+  likes,
 }: {
   title: string;
   discription?: string;
   source: "chan_4" | "subreddits" | "pinterest" | "twitter";
+  media?: any;
+  likes?: number;
 }) => {
   const [selected, setSelected] = useAtom(payloadForL1);
   const handleSelect = () => {
@@ -44,9 +50,19 @@ export const CardBox = ({
       overflow="hidden"
       variant="outline"
       bg={selected[source].includes(title as never) ? "green.600" : ""}
+      // maxW={"xl"}
     >
       <Stack>
         <CardBody>
+          <Box>
+            {source === "twitter" && media && media!.fullUrl && (
+              <>
+                <Image src={media.fullUrl} alt={title} w={200} h={200} />
+                <Text py="2">{likes} likes</Text>
+              </>
+            )}
+          </Box>
+
           <Heading size="md">{title}</Heading>
 
           {discription && <Text py="2">{discription}</Text>}
